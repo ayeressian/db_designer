046256a49daaac0285d72f75261d6b8c0d51cef5
Ara Yeressian <ara_yeressian@epam.com>
2017-12-08 20:07:03 +0400

    .

--- script.js
+++ script.js
@@ -23,32 +23,32 @@ const table2 = new Table({
   }
 });
 
-// const table3 = new Table({
-//   name: 'CCC',
-//   columns: [
-//     { name: "id", type: "INT" },
-//     { name: "AAA_id", type: "INT", fk: { table, column: table.columns[0] } }
-//   ],
-//   pos: {
-//     x: 400,
-//     y: 200
-//   }
-// });
+const table3 = new Table({
+  name: 'CCC',
+  columns: [
+    { name: "id", type: "INT" },
+    { name: "AAA_id", type: "INT", fk: { table, column: table.columns[0] } }
+  ],
+  pos: {
+    x: 400,
+    y: 200
+  }
+});
 
-// const table4 = new Table({
-//   name: 'EEE',
-//   columns: [
-//     { name: "id", type: "INT" },
-//     { name: "CCC_id", type: "INT", fk: { table, column: table3.columns[0] } }
-//   ],
-//   pos: {
-//     x: 250,
-//     y: 350
-//   }
-// });
+const table4 = new Table({
+  name: 'EEE',
+  columns: [
+    { name: "id", type: "INT" },
+    { name: "CCC_id", type: "INT", fk: { table, column: table3.columns[0] } }
+  ],
+  pos: {
+    x: 250,
+    y: 350
+  }
+});
 
-// const designer = new Designer([table, table2, table3, table4]);
+const designer = new Designer([table, table2, table3, table4]);
 
-const designer = new Designer([table, table2]);
+// const designer = new Designer([table, table2]);
 
 window.designer = designer;
